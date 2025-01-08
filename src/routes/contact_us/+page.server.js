14f030ff7be212f2dddedca88fd92c86d1cd7f1a
Ayush Kumar <ayushsuperstar48@gmail.com>
2025-01-08 21:35:26 +0530

    email service added

--- src/routes/contact_us/+page.server.js
+++ src/routes/contact_us/+page.server.js
@@ -1,3 +1,6 @@
+import nodemailer from 'nodemailer';
+import { EMAIL_PASS, EMAIL_USER } from '$env/static/private';
+
 export const actions = {
 	default: async ({ request }) => {
 		const data = await request.formData();
@@ -6,7 +9,25 @@ export const actions = {
 		const subject = data.get('subject');
 		const number = data.get('number');
 		const message = data.get('message');
+		try {
+			const transporter = nodemailer.createTransport({
+				service: 'Gmail',
+				auth: {
+					user: EMAIL_USER,
+					pass: EMAIL_PASS
+				}
+			});
 
-		return { success: true };
+			await transporter.sendMail({
+				from: email,
+				to: EMAIL_USER,
+				subject,
+				text: `You have received a new message from ${name} \nEmail:- ${email} \nPhone:- ${number} \n\nMessage:-\n${message}`
+			});
+
+			return { success: true };
+		} catch (err) {
+			console.log('Error while submitting form: ', err);
+		}
 	}
 };
